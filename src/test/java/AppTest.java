f8bbbbf1e3acd294018c223728cb69003a139026
Adam Craig <ajcraig@suffolk.edu>
2016-04-22 11:02:13 -0700

    add integration testing to project via addition of FluentLenium files, including proper imports in App.java, AppTest.java and build.gradle. Add template and home.vtl contents. Add first FluentLenium integration test for accessing the homepage

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,7 +1,30 @@
-import org.junit.*;
+import org.fluentlenium.adapter.FluentTest;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import static org.junit.Assert.*;
+import org.openqa.selenium.By;
 
-public class AppTest {
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
+  public WebDriver getDefaultDriver() {
+    return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("You may not know it, but you've entered Puzzle Land (TM).");
+  }
 
   @Test
   public void puzzleMaker_forAnyStringInputWithoutVowels_string() {
@@ -30,6 +53,7 @@ public class AppTest {
   @Test
   public void solveThePuzzle_forExactGuessMatch_stringSayingThatsCorrect() {
     App testPuzzle = new App();
+    String inputFromPuzzleMaker = testPuzzle.puzzleMaker("puppy");
     assertEquals("That's correct!", testPuzzle.solveThePuzzle("puppy"));
   }
 }
