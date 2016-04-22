c13dc75990cb234e1fd76c1486d619821db6186a
Adam Craig <ajcraig@suffolk.edu>
2016-04-22 08:26:00 -0700

    add preliminary contents of App.java and AppTest.java, so that the first gradle test builds successfully without any actual tests yet

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,18 +1,9 @@
-apply plugin: 'java'
-apply plugin: 'application'
+public class App {
+  public static void main(String[] args) {}
 
-archivesBaseName = "dash-puzzle"
-version = '1.0'
-mainClassName = "App"
+  public String puzzleMaker(String input) {
 
-repositories {
- mavenCentral()
-}
-
-dependencies {
-  compile group: 'com.sparkjava', name: 'spark-core', version: '2.3'
-  compile group: 'org.apache.velocity', name: 'velocity', version: '1.7'
-  testCompile group: 'junit', name: 'junit', version: '4.+'
-  testCompile group: 'org.fluentlenium', name: 'fluentlenium-core', version: '0.10.3'
-  testCompile group: 'org.fluentlenium', name: 'fluentlenium-assertj', version: '0.10.3'
+    //
+    return "";
+  }
 }
