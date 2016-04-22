3346a5ec2962f825ceac6b999b3981acca6402ea
Adam Craig <ajcraig@suffolk.edu>
2016-04-22 09:16:49 -0700

    add passing App.java code for test forASingleWordInputWithVowels

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,18 @@
+import java.util.Arrays;
+
 public class App {
   public static void main(String[] args) {}
 
   public String puzzleMaker(String input) {
 
-    //
-    return input;
+    String puzzleMadeFromInput = input;
+
+    puzzleMadeFromInput = puzzleMadeFromInput.replaceAll("a", "-");
+    puzzleMadeFromInput = puzzleMadeFromInput.replaceAll("e", "-");
+    puzzleMadeFromInput = puzzleMadeFromInput.replaceAll("i", "-");
+    puzzleMadeFromInput = puzzleMadeFromInput.replaceAll("o", "-");
+    puzzleMadeFromInput = puzzleMadeFromInput.replaceAll("u", "-");
+
+    return puzzleMadeFromInput;
   }
 }
