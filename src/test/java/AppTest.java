6e686dc3e9dde0a2650a554a48379cf3cb13c198
Adam Craig <ajcraig@suffolk.edu>
2016-04-22 11:03:57 -0700

    add failing code for rootTest integration test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest extends FluentTest {
   @Test
   public void rootTest() {
     goTo("http://localhost:4567/");
-    assertThat(pageSource()).contains("You may not know it, but you've entered Puzzle Land (TM).");
+    assertThat(pageSource()).contains("Yo may not know it, but you've entered Puzzle Land (TM).");
   }
 
   @Test
